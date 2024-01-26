d4ec64d3ffbce11eb86e66d2cbd2e62d11236526
shivam <76024545+dextroshivam@users.noreply.github.com>
2024-01-26 15:09:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	let arr={};
+    for(let i=0;i<str.length;i++){
+        if(arr[str[i]]==undefined){
+            arr[str[i]]=1;
+        }else{
+            arr[str[i]]+=1;
+        }
+    }
+    console.log(arr);
+    for(let i=0;i<str.length;i++){
+        if(arr[str[i]]==1) return str[i];
+    }
+    return null;
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
